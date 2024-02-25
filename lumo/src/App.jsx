7e1983df1048e6b26ecacdf6ad064d311490c8a0
Vishal Agarwal <a.vishal@wustl.edu>
2024-02-25 16:06:44 -0600

    basic login stuff

--- lumo/src/App.jsx
+++ lumo/src/App.jsx
@@ -1,60 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { createClient } from "@supabase/supabase-js";
-import './App.css'
+
+  import './index.css'
+  import { useState, useEffect } from 'react'
+  import { createClient } from '@supabase/supabase-js'
+  import { Auth } from '@supabase/auth-ui-react'
+  import { ThemeSupa } from '@supabase/auth-ui-shared'
 
   const supabase = createClient("https://dzmmfskrxgkcjakmhutk.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6bW1mc2tyeGdrY2pha21odXRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg4OTQ5NTksImV4cCI6MjAyNDQ3MDk1OX0.rcnmUdhmLpXxlhgkPAUq1jA743biNbMLZtOtR361AS0");
 
+  export default function App() {
+    const [session, setSession] = useState(null)
 
-function App() {
-  const [count, setCount] = useState(0)
+    useEffect(() => {
+      supabase.auth.getSession().then(({ data: { session } }) => {
+        setSession(session)
+      })
 
-  const [countries, setCountries] = useState([]);
+      const {
+        data: { subscription },
+      } = supabase.auth.onAuthStateChange((_event, session) => {
+        setSession(session)
+      })
 
-  // Sample pulling from countries supabase data!
-  //   useEffect(() => {
-  //     getCountries();
-  //   }, []);
+      return () => subscription.unsubscribe()
+    }, [])
 
-  //   async function getCountries() {
-  //     const { data } = await supabase.from("countries").select();
-  //     setCountries(data);
-  //   }
-
-  //   return (
-  //     <ul>
-  //       {countries.map((country) => (
-  //         <li key={country.name}>{country.name}</li>
-  //       ))}
-  //     </ul>
-  //   );
-  // }
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    if (!session) {
+      return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+    }
+    else {
+      return (<div>Logged in!</div>)
+    }
+  }
