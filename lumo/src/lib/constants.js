66e0518a884147117443bbea8b1fc3b10d964408
Vishal Agarwal <a.vishal@wustl.edu>
2024-02-25 15:42:16 -0600

    set up supabase

--- lumo/src/lib/constants.js
+++ lumo/src/lib/constants.js
@@ -1,2 +1,2 @@
-export const REACT_APP_SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
-export const REACT_APP_SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY;
+export const REACT_APP_SUPABASE_URL = 'https://dzmmfskrxgkcjakmhutk.supabase.co';
+export const REACT_APP_SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6bW1mc2tyeGdrY2pha21odXRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg4OTQ5NTksImV4cCI6MjAyNDQ3MDk1OX0.rcnmUdhmLpXxlhgkPAUq1jA743biNbMLZtOtR361AS0';
