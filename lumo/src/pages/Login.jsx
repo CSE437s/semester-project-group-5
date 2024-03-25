8fc04044168d1276ef6e95807694e3e18bab1786
bashar43 <89860891+bashar43@users.noreply.github.com>
2024-03-25 18:08:55 -0500

    login fixed

--- lumo/src/pages/Login.jsx
+++ lumo/src/pages/Login.jsx
@@ -8,24 +8,13 @@ import PasswordInput from "../components/PasswordInput";
 import Logo from "../components/Logo";
 import Link from "../components/Link";
 
-import { useMutation } from "@tanstack/react-query";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { supabase } from "../supabase";
 import { useSession } from "../services/api";
 
 export default function Login() {
   const session = useSession();
-
-  const { mutate: login, isLoading } = useMutation(async (formData) => {
-    const { error } = await supabase.auth.signInWithPassword({
-      email: formData.get("email"),
-      password: formData.get("password"),
-    });
-
-    if (error) {
-      console.error(error);
-    }
-  });
-
   // Navigate back to the home page on login.
   if (session) {
     return <Navigate to="/About" replace />;
@@ -36,33 +25,12 @@ export default function Login() {
       <Container maxWidth="xs">
         <Box display="flex" flexDirection="column" alignItems="center">
           <Logo />
-
-          <Form method="post" action={login} sx={{ marginTop: 4 }}>
-            <EmailInput autoFocus fullWidth />
-            <PasswordInput fullWidth margin="normal" />
-
-            <Button
-              type="submit"
-              variant="contained"
-              disabled={isLoading}
-              fullWidth
-              sx={{ marginTop: 3 }}
-            >
-              Sign In
-            </Button>
-          </Form>
-
-          <Link to="/forgot" color="text.secondary" sx={{ marginTop: 2 }}>
-            Forgot your password?
-          </Link>
+          <Auth
+            supabaseClient={supabase}
+            appearance={{ theme: ThemeSupa }}
+            providers={["google"]}
+          />
         </Box>
-
-        <Typography marginTop={4}>
-          Don't have an account?{" "}
-          <Link to="/register" color="primary.light">
-            Sign up
-          </Link>
-        </Typography>
       </Container>
     </Box>
   );
