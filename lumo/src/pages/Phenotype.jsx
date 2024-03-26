2a81207bf356b9838edf5cb06aff7d1899d4f085
bashar43 <89860891+bashar43@users.noreply.github.com>
2024-03-26 16:02:34 -0500

    chart working

--- lumo/src/pages/Phenotype.jsx
+++ lumo/src/pages/Phenotype.jsx
@@ -1,4 +1,5 @@
 import { Stack, Typography, Button, getGrid2UtilityClass } from "@mui/material";
+import { RadarChart, Radar, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Legend } from "recharts";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { supabase } from "../supabase";
@@ -29,22 +30,55 @@ export default function Phenotype() {
       if (responses && responses.length > 0) {
         setResponses(responses);
         setResultsAvailable(true);
-        getGraphData(responses[0].factorScores);
+        setGraphData(getGraphData(responses[0].factorScores));
       }
     }
     fetchResponses();
   }, []);
 
   function getGraphData(factorScores) {
-    // Extract data
+    // Extracted data
     let factorNames = [];
     let factorRatios = [];
+
     for (var i = 0; i < factorScores.length; i++) {
       factorNames.push(factorScores[i].name);
       factorRatios.push(100 * (factorScores[i].responseScore / factorScores[i].totalScore));
     }
-    console.log(factorNames);
-    console.log(factorRatios);
+
+    const data = [
+      {
+        subject: factorNames[0],
+        A: factorRatios[0],
+        fullMark: 100,
+      },
+      {
+        subject: factorNames[1],
+        A: factorRatios[1],
+        fullMark: 100,
+      },
+      {
+        subject: factorNames[2],
+        A: factorRatios[2],
+        fullMark: 100,
+      },
+      {
+        subject: factorNames[3],
+        A: factorRatios[3],
+        fullMark: 100,
+      },
+      {
+        subject: factorNames[4],
+        A: factorRatios[4],
+        fullMark: 100,
+      },
+      {
+        subject: factorNames[5],
+        A: factorRatios[5],
+        fullMark: 100,
+      },
+    ];
+    return data;
   }
 
   return (
@@ -65,8 +99,23 @@ export default function Phenotype() {
             Your Financial Phenotype!
           </Typography>
           <Typography maxWidth="sm" variant="body1" marginTop={2}>
-            Here are your results
+            Here are your results:
           </Typography>
+
+          <RadarChart outerRadius={85} width={730} height={250} data={graphData}>
+            <PolarGrid />
+            <PolarAngleAxis dataKey="subject" />
+            <PolarRadiusAxis angle={30} domain={[0, 100]} />
+            <Radar
+              name="Financial Fingerprint"
+              dataKey="A"
+              stroke="#8884d8"
+              fill="#8884d8"
+              fillOpacity={0.6}
+            />
+            <Legend />
+          </RadarChart>
+
           {responses[0].factorScores.map((factor) => (
             <div key={factor.name}>
               <p>
