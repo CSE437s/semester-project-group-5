fe61420ce02f32262cf37f5b530761e51c3b28e9
bashar43 <89860891+bashar43@users.noreply.github.com>
2024-04-02 14:05:00 -0500

    theme + navbar tweak

--- lumo/src/main.jsx
+++ lumo/src/main.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "./components/SessionProvider.jsx";
+import { ThemeProvider } from "@mui/material";
 
 import App from "./App.jsx";
+import theme from "./theme.js";
 import "./index.css";
 
 const queryClient = new QueryClient({
@@ -17,9 +19,11 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <SessionProvider>
-        <App />
-      </SessionProvider>
+      <ThemeProvider theme={theme}>
+        <SessionProvider>
+          <App />
+        </SessionProvider>
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
