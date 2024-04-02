445b22085d2e7585c4ab247eb06ddad47119409b
David Anyaeche <david_anyaeche@davids-mbp-6.dhcp.wustl.edu>
2024-04-02 14:42:16 -0500

    App.jsx rentvsbuy route

--- lumo/src/App.jsx
+++ lumo/src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
-import { LayoutNav } from './pages/LayoutNav';
+import LayoutNav from './pages/LayoutNav'; // Adjusted for default import
 import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Phenotype from './pages/Phenotype';
 import Quiz from './pages/Quiz';
-import SavingsCalculatorPage from './pages/SavingsCalculatorPage'; // Import the SavingsCalculatorPage
+import SavingsCalculatorPage from './pages/SavingsCalculatorPage';
+import RentVsBuyCalculatorPage from './pages/RentVsBuyCalculatorPage';
 
 import { supabase } from './supabase';
 import { createClient } from '@supabase/supabase-js';
@@ -22,10 +23,10 @@ export default function App() {
           <Route path="about" element={<About />} />
           <Route path="phenotype" element={<Phenotype />} />
           <Route path="quiz" element={<Quiz />} />
-          {/* Update the route for /home/nowvslater to use SavingsCalculatorPage */}
           <Route path="nowvslater" element={<SavingsCalculatorPage />} />
+          <Route path="rentvsbuy" element={<RentVsBuyCalculatorPage />} />
         </Route>
-        {/* You can add more routes as needed */}
+        {/* Add other routes here as needed */}
       </Routes>
     </Router>
   );
